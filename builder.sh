a5fa2d54d916edaf74b096edd02ffc1ca4beae37
Artem <marisov.av@gmail.com>
2023-09-27 20:46:47 -0400

    Fixed builder script

--- builder.sh
+++ builder.sh
@@ -1,10 +1,10 @@
-# 
+#! /bin/bash
 
 SCRIPT_PATH=`realpath $0`
 CLUSTERGEN_DIR=`dirname $SCRIPT_PATH`
 
 if [ -z "$BUILD_RPM"  ] ; then
- echo " 
+  echo " 
 ************************************************************
 ** IMPORTANT NOTICE **
 ************************************************************
@@ -19,11 +19,11 @@ Would you like to build Slurm now?
 
 Please enter 'y' to build it.
 "
- 
+  read BUILD_RPM
 fi
 
 python3 cgen.py -i $CLUSTERGEN_DIR/cluster.json -d $CLUSTERGEN_DIR/build -f 
 
 if [ "$BUILD_RPM" == "y" ] ; then
-  $CLUSTERGEN_DIR/build/scripts/install.sh
+  $CLUSTERGEN_DIR/build/scripts/buildslurm.sh
 fi
